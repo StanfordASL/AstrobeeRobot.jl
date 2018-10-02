9092be9e1c012c2c8ee404cf64a83b6a5eee74df
Abhishek Cauligi <acauligi@stanford.edu>
2018-10-02 10:41:58 -0700

    Initial commit with mesh files

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # AstrobeeRobot
+Mesh files imported from [Astrobee Media](https://github.com/nasa/astrobee_media) repository and converted to *.obj files
--- src/AstrobeeRobot.jl
+++ src/AstrobeeRobot.jl
@@ -1,5 +1,44 @@
+__precompile__()
+
 module AstrobeeRobot
 
-# package code goes here
+using RigidBodyDynamics
+using StaticArrays
+
+export Astrobee,
+  packagepath,
+  urdfpath
+
+packagepath() = joinpath(@__DIR__, "..", "deps")
+urdfpath() = joinpath(packagepath(), "Astrobee", "astrobee.urdf")
+
+mutable struct Astrobee{T}
+  mechanism::Mechanism{T}
+  basejoint::Joint
+  body::RigidBody
+
+  function Astrobee{T}(;floating=true) where T
+    mechanism = RigidBodyDynamics.parse_urdf(T,urdfpath())
+
+    #create floating joint
+    body = findbody(mechanism, "body") 
+    basejoint = joint_to_parent(body,mechanism)
+    if floating
+     println("instantiated a floating joint")
+     floatingjoint = Joint(basejoint.name, frame_before(basejoint), frame_after(basejoint), QuaternionFloating{T}())
+     replace_joint!(mechanism, basejoint, floatingjoint)
+     basejoint=floatingjoint
+   end
+
+  new{T}(mechanism, basejoint, body)
+  end
+end
+Astrobee(::Type{T} = Float64; kwargs...) where {T} = Astrobee{T}(; kwargs...)
+
+function __init__()
+  if !isfile(urdfpath())
+    error("Could not find $(urdfpath()). Please run `import Pkg; Pkg.build(\"Astrobee\")`.")
+  end
+end
 
 end # module
